842dec5bbb5c6a37f97ab5c05d681c26808c7551
MrCamoga <mrcamoga@gmail.com>
2025-03-28 19:44:05 +0100

    Pokimon

--- scripts.js
+++ scripts.js
@@ -1,1 +1,19 @@
 console.log(document.title);
+
+document.getElementById("gen-1").textContent = "Generasión 1 Pokimon";
+
+document.getElementById("gen-1").nextElementSibling.style.backgroundColor = "#ff0000";
+
+console.log(document.documentURI);
+console.log(document.domain);
+
+let imgs = document.getElementsByTagName("img");
+for (let i = 0; i < imgs.length; i++) {
+	console.log(imgs[i]);
+	imgs[i].setAttribute("src", "https://media.giphy.com/media/2v170e71aanfi/giphy.gif");
+}
+
+let flying = document.querySelectorAll(".infocard-lg-data.text-muted small a.itype.flying");
+for (let i = 0; i < flying.length; i++) {
+	flying[i].closest(".infocard-lg-data.text-muted").style.backgroundColor = "#9030d0";
+}
