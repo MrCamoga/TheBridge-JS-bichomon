2a9f645deb179d1d775cc3ac13c16b866a8868c7
MrCamoga <mrcamoga@gmail.com>
2025-03-31 20:14:40 +0200

    contar palabras 3

--- practica1.js
+++ practica1.js
@@ -1,5 +1,3 @@
-// PRACTICA 1
-
 function splitString(str) {
 	return str.toUpperCase().split("").join(" ");
 }
@@ -25,10 +23,22 @@ function countWords2(str) {
 	return count;
 }
 
+// functiona con multiples espacios
+function countWords3(str) {
+	let count = 0;
+	let space = true;
+	for (let i = 0; i < str.length; i++) {
+		count += space && str[i] !== " ";
+		space = str[i] === " ";
+	}
+	return count;
+}
+
 console.log(splitString("ertrghjfioentoe"));
 console.log(splitString2("ertrghjfioentoe"));
 console.log(countWords("Carlos Moya García"));
 console.log(countWords2("Carlos Moya García"));
+console.log(countWords3("  Carlos Moya García   "));
 console.log(splitString(""));
 console.log(splitString2(""));
 console.log(countWords(""));
